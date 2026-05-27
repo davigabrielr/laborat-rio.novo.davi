2f51acb26f2628797d4b004271f0ea656c7f1521
davigabrielr <davi.roza@amf.edu.br>
2026-05-26 22:19:25 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,14 @@
+
+def valor(numero):
+    dobro = numero * 2
+    print("dobro do numero:", dobro)
+    return dobro
+def valor(numero):
+    triplo = numero * 3
+    print("triplo do numero:", triplo)
+    return triplo
+
+def main():
+    valor = float(input("digite seu numero:"))
+#terminar
+main()
